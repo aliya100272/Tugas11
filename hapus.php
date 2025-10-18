5c33e4c8ff5b50c2d2011907f76e5c620f0f789e
Mudri Katul Aliyah <mdrktul02aliya@gmail.com>
2025-10-18 22:22:58 +0700

    update

--- hapus.php
+++ hapus.php
@@ -1,7 +1,12 @@
 <?php
-session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
 include "koneksi.php";
 $id = $_GET['id'];
 
